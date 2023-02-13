933cbc5bf1101bbdbdaafd584d4d1a160f7186c7
Titwech Wal <Titwech.wal@stu.ocsb.ca>
2023-02-13 18:29:59 +0000

    added style

---
+++ HelloWorldStyle.java
@@ -0,0 +1,31 @@
+/**
+* Code describing what you plan to do
+* Next year and how you feel about it.
+*
+* @author Titwech Wal
+* @version 1.0
+* @since   2023-02-08
+*/
+public final class HelloWorldStyle {
+
+    /**
+    * This is a private constructor used to satisfy
+    * the style checker.
+    *
+    * @exception IllegalStateException Utility class.
+    * @see IllegalStateException
+    */
+    private HelloWorldStyle() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    /**
+    * This is the main method.
+    *
+    * @param args Unused.
+    */
+    public static void main(String[] args) {
+        System.out.println("I plan to go to Emilycarr university");
+    }
+}
+
